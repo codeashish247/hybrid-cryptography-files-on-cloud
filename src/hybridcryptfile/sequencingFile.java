ba5d48d81e6b3ba2b75b7e5e54a423cc2d89ced1
ashishranjan <ashishranjan@localhost>
2020-04-17 18:10:16 +0530

    firstCommit

---
+++ src/hybridcryptfile/sequencingFile.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package hybridcryptfile;
+import java.io.*;
+import java.util.*;
+    
+
+/**
+ *
+ * @author ashishranjan
+ */
+public class sequencingFile {
+    public void sequenceFind(String fileN) throws FileNotFoundException, IOException{
+        try{
+            char ch;
+            int i,charCount=0;
+            FileReader fr = new FileReader(fileN);
+            while((i=fr.read())!=-1){
+                if(i==10){
+                    System.out.println(charCount);
+                    charCount=0;
+                }else{
+                    ++charCount;
+                    System.out.print((char)i);
+                }
+            }
+        }catch(FileNotFoundException fe){
+            System.out.println("File Not Found!");
+        }  
+    }
+}
